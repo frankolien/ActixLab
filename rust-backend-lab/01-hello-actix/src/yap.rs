636f6302037fb4f9097f2ab3f65d89325f75d245
frankolien <frankolien123@gmail.com>
2026-07-13 12:15:30 +0100

    Initial commit

---
+++ rust-backend-lab/01-hello-actix/src/yap.rs
@@ -0,0 +1,6 @@
+// hello actix, now wtf do i need to get started 
+// hello actix features:Routing, extractors, app state, configuration with dotenvy.
+// it teaches : the Actix request lifecycle, App and HttpServer setup
+// now what cool project i can use for this example 
+// hmm coffee shop api it is 
+// so buy, sell,create order, menu 
